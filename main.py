812f1b37f3f7eb64d201a0f918e493f5ed429819
Nick Salt <46006335+BlueBerrySans365@users.noreply.github.com>
2023-11-27 23:47:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# Я буду писать всё чтобы нахуй трезвым не ахуеть после пъянки
+
+# Импорт модулей
 import logging
+import sqlite3 
+
 from aiogram import F, Bot, Dispatcher, types
 from config import BOT_TOKEN
 from aiogram.fsm.context import FSMContext
@@ -9,12 +14,16 @@ from aiogram.types import (
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import Command
 
-
+# Дебаг хуйня TODO: включение на аргумент
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] {%(filename)s:%(funcName)s:%(lineno)d} %(levelname)s - %(message)s",
     datefmt="%H:%M:%S",
 )
+
+con = sqlite3.connect("defaulters.db")
+cur = con.cursor()
+
 
 storage = MemoryStorage()
 bot = Bot(BOT_TOKEN, parse_mode="html")
