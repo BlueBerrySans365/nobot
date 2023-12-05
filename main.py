b15eb8ce071aa9aab4228bccff2c3671d151fb08
Nick Salt <46006335+BlueBerrySans365@users.noreply.github.com>
2023-12-06 00:07:00 +0200

    аААААААААААа
    Я переработал тему, терь надо добавить ДБ. Пока добавь пару новых команд

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Импорт модулей
 import logging
 import sqlite3
+import sys
 
 from aiogram import F, Bot, Dispatcher, types
 from config import BOT_TOKEN, GROUP_ID
@@ -15,12 +16,6 @@ from aiogram.types import (
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import Command
 
-# Дебаг хуйня TODO: включение на аргумент
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s] {%(filename)s:%(funcName)s:%(lineno)d} %(levelname)s - %(message)s",
-    datefmt="%H:%M:%S",
-)
 
 con = sqlite3.connect("defaulters.db")
 cur = con.cursor()
@@ -29,27 +24,41 @@ cur = con.cursor()
 storage = MemoryStorage()
 bot = Bot(BOT_TOKEN, parse_mode="html")
 dp = Dispatcher(storage=storage)
+try:
+    if sys.argv[1] == "--fuckery":
+    # Дебаг хуйня TODO: включение на аргумент
+        logging.basicConfig(
+            level=logging.INFO,
+            format="[%(asctime)s] {%(filename)s:%(funcName)s:%(lineno)d} %(levelname)s - %(message)s",
+            datefmt="%H:%M:%S",
+        )
 
-
-@dp.message(Command("id"))
-async def get_id(message: Message, state: FSMContext):
-    await message.answer(str(message.chat.id))
+        @dp.message(Command("id"))
+        async def get_id(message: Message, state: FSMContext):
+            await message.answer(str(message.chat.id))
+    else:
+        pass
+except Exception:
+    pass
 
 @dp.message()
-async def check_membership(message: types.Message):
-    user_id = message.from_user.id
-    chat_id = GROUP_ID
-
+async def base_shit(message: types.Message):
     # Проверяем, является ли пользователь участником группы
-    chat_member = await bot.get_chat_member(chat_id, user_id)
-    if chat_member.status in ['member', 'administrator', 'creator']:
-        # Пользователь является участником группы
-        await message.answer("идите нахуй, лол")
-    else:
-        # Пользователь не является участником группы
-        pass
+    match message.chat.id:
+        case GROUP_ID:
+            try:
+                match message.text.lower():
+                    case "иди нахуй":
+                        await message.reply("Сам иди бля, быдло")
+                    case _:
+                        pass
+            except Exception:
+                pass # Бля, денис, я явно набухался, будь так добр, распиши некоторые команды боту
+
+
 
 
 
 if __name__ == "__main__":
+
     dp.run_polling(bot)
